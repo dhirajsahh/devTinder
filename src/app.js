ec0dc04f0806248534419773d3f5d7f503b3c6b3
dhirajsahh <dhirajmahato2019@gmail.com>
2025-12-03 07:43:25 +0545

    exploring different routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 
 const app = express();
-app.use("/api", (req, res) => {
-  res.send("Hello army");
-});
-app.use("/", (req, res) => {
-  res.send("Hello world123");
-});
-
+app.get('/user',(req,res)=>{
+    res.send({
+      "fristName":"Dhiraj",
+      "lastName":"Mahato"
+    })
+   
+    
+})
+app.post("/user",(req,res)=>{
+    res.send("User created successfully")
+})
+app.patch("/user",(req,res)=>{
+    res.send("Email updated successfully")
+})
+app.delete("/user",(req,res)=>{
+    res.send("User deleted successfully");
+})
 app.listen(3000, () => {
   console.log("App is listening in port 3000");
 });
