b1e74bb901a4cae20d14bd4a844988d01f2b75b8
dhirajsahh <dhirajmahato2019@gmail.com>
2025-12-03 07:46:19 +0545

    exploring different routes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 
 const app = express();
-app.get('/user',(req,res)=>{
-    res.send({
-      "fristName":"Dhiraj",
-      "lastName":"Mahato"
-    })
-   
-    
-})
-app.post("/user",(req,res)=>{
-    res.send("User created successfully")
-})
-app.patch("/user",(req,res)=>{
-    res.send("Email updated successfully")
-})
-app.delete("/user",(req,res)=>{
-    res.send("User deleted successfully");
-})
+app.get("/user", (req, res) => {
+  res.send({
+    fristName: "Dhiraj",
+    lastName: "Mahato",
+  });
+});
+app.post("/user", (req, res) => {
+  res.send("User created successfully");
+});
+app.patch("/user", (req, res) => {
+  res.send("Email updated successfully");
+});
+app.put("/user", (req, res) => {
+  res.send("user detail is replace successfully");
+});
+app.delete("/user", (req, res) => {
+  res.send("User deleted successfully");
+});
 app.listen(3000, () => {
   console.log("App is listening in port 3000");
 });
