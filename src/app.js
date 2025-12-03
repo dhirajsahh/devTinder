80c6bd270862c4864a4b2aff14d67b2c0789ee9e
dhirajsahh <dhirajmahato2019@gmail.com>
2025-12-03 16:07:16 +0545

    handling multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,29 +1,75 @@
 const express = require("express");
 
 const app = express();
-// app.get(/us(er)+/, (req, res) => {
-//   res.send({
-//     fristName: "Dhiraj",
-//     lastName: "Mahato",
-//   });
-// });
-// app.get(/user/, (req, res) => {
-//   res.send({
-//     fristName: "Dhiraj",
-//     lastName: "Mahato",
-//   });
-// });
-// app.get("/us*er", (req, res) => {
-//   res.send({
-//     fristName: "Dhiraj",
-//     lastName: "Mahato",
-//   });
-// });
-// app.get("/user",(req,res)=>{
-//     res.send("Hello user")
-//     console.log(req.query);
+
+app.get("/user",[(req,res,next)=>{
+  console.log("Handling the user route 1");
+  //next();
+  //res.send("Frist response")
+  next();
+},
+(req,res,next)=>{
+  console.log("Handling the user Route2");
+ // res.send("2nd response");
+  next();
+},
+(req,res,next)=>{
+  console.log("Handling the user Route 3");
+  next();
+  //res.send("third response");
+},
+(req,res,next)=>{
+  console.log("Handing the user Route 4");
+  res.send("fourth Response")
+ // next();
+  
+}]
+
+)
+
+/*app.use("/user",(req,res,next)=>{
+  console.log("First response");
+  if(req.query.userId==100){
+    res.send("first Response");
+  }
+  else next()
+},
+(req,res)=>{
+  console.log("second Respose");
+  res.send("Second response");
+},
+(req,res)=>{
+  
+}
+)
+*/
+app.listen(3000, () => {
+  console.log("App is listening in port 3000");
+});
+/*
+app.get(/us(er)+/, (req, res) => {
+  res.send({
+    fristName: "Dhiraj",
+    lastName: "Mahato",
+  });
+});
+app.get(/user/, (req, res) => {
+  res.send({
+    fristName: "Dhiraj",
+    lastName: "Mahato",
+  });
+});
+app.get("/us*er", (req, res) => {
+  res.send({
+    fristName: "Dhiraj",
+    lastName: "Mahato",
+  });
+});
+app.get("/user",(req,res)=>{
+    res.send("Hello user")
+    console.log(req.query);
     
-// })
+})
 app.get("/user",(req,res)=>{
     res.send("Hello user")
    
@@ -46,6 +92,5 @@ app.put("/user", (req, res) => {
 app.delete("/user", (req, res) => {
   res.send("User deleted successfully");
 });
-app.listen(3000, () => {
-  console.log("App is listening in port 3000");
-});
+
+*/
