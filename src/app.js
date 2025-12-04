10709e8c24681f8e93399e8c698b7b51a09b76b3
dhirajsahh <dhirajmahato2019@gmail.com>
2025-12-04 17:11:50 +0545

    Handling different routes

--- src/app.js
+++ src/app.js
@@ -3,22 +3,15 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 const Port = 3000;
-
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const userDetails = {
-    //firstName: "virat",
-    lastName: "kohli",
-    email: "vk@gmail.com",
-    password: "anuksa",
-    age: 30,
-    gender: "Male",
-  };
+  const userDetails = req.body;
   try {
-    const doc = new User(userDetails);
-    await doc.save();
+    const userData = new User(userDetails);
+    await userData.save();
     res.json({
       message: "User added successfully",
-      user: doc,
+      user: userData,
     });
   } catch (err) {
     console.log("Error", err.message);
@@ -29,6 +22,75 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/getUser", async (req, res) => {
+  const email = req.body.email;
+  try {
+    const user = await User.findOne({ email });
+    res.send(user);
+  } catch (err) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+});
+app.get("/getUserById", async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const user = await User.findById(userId);
+    res.send(user);
+  } catch (err) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+});
+app.patch("/updateUser", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { email: email },
+      { firstName: name },
+      { upsert: true, returnDocument: "after" }
+    );
+    console.log(updatedUser);
+
+    res.send("User updated successfully");
+  } catch (err) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+});
+app.delete("/deleteById/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    const data = await User.findByIdAndDelete(id);
+    console.log(data);
+    res.send("user is deleted Successfully");
+  } catch (err) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+});
+app.delete("/delete", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const data = await User.findOneAndDelete({ email });
+    console.log(data);
+    res.send("user is deleted Successfully");
+  } catch (err) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+});
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
