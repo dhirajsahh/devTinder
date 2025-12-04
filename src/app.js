7ba40e275198bc872b661da23c9029215cf5d5f8
dhirajsahh <dhirajmahato2019@gmail.com>
2025-12-04 07:22:28 +0545

    database initlization

--- src/app.js
+++ src/app.js
@@ -1,42 +1,41 @@
+const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
+const app = express();
+const Port = 3000;
 
-const express=require('express');
-const {isAuthneticated,isUserAuthneticated}=require('./middlewares/admin_auth')
-const app=express();
-const Port=3000;
-app.use("/admin",isAuthneticated)
-app.post("/user/login",(req,res)=>{
-  res.send("User login successfully");
-})
-app.get("/user/getData",isUserAuthneticated,(req,res)=>{
-  res.send("User data");
-})
-app.get("/admin/getAllData",(req,res)=>{
+app.post("/signup", async (req, res) => {
+  const userDetails = {
+    //firstName: "virat",
+    lastName: "kohli",
+    email: "vk@gmail.com",
+    password: "anuksa",
+    age: 30,
+    gender: "Male",
+  };
+  try {
+    const doc = new User(userDetails);
+    await doc.save();
+    res.json({
+      message: "User added successfully",
+      user: doc,
+    });
+  } catch (err) {
+    console.log("Error", err.message);
+    res.json({
+      message: "Error while putting data in database",
+      error: err.message,
+    });
+  }
+});
 
-  res.send("All the data")
-   /*const bodyToken="XYZ";
-   const actualToken="XYZ1";
-   if(bodyToken===actualToken){
-       res.send("send all Data")
-   }
-   else {
-    res.status(401).send("Your are not authorized")
-   }
-    */
-   
-})
-app.delete("/admin/deleteData",(req,res)=>{
-  res.send("Data deleted");
-   /*const bodyToken="XYZ";
-   const actualToken="XYZ1";
-   if(bodyToken===actualToken){
-       res.send(" Data Deleted")
-   }
-   else {
-    res.status(401).send("Your are not authorized")
-   }
-    */
-})  
-
-app.listen(Port,()=>{
-  console.log(`App is listening in Port ${Port}`)
-})
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(Port, () => {
+      console.log(`App is listening in Port ${Port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Error connecting the database");
+  });
