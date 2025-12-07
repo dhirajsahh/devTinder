c64afd3ca997e3eeee45a388300180edff1c612a
dhirajsahh <dhirajmahato2019@gmail.com>
2025-12-07 07:18:11 +0545

    user feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/admin_auth");
 const { ConnectionRequestModel } = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 //get all the pending request for the logged in user
@@ -44,6 +45,42 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       message: "connection list",
       data,
     });
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+    console.log(err);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  try {
+    //User should see all the user cards expect
+    //  his own card
+    // his connections
+    // ignored people
+    // already sent the connection request
+    const loggedInUserId = req.user._id;
+
+    const connectionRequest = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUserId }, { toUserId: loggedInUserId }],
+    }).select("fromUserId toUserId");
+
+    const hideFromUser = new Set();
+
+    connectionRequest.map((req) => {
+      hideFromUser.add(req.toUserId._id.toString());
+      hideFromUser.add(req.fromUserId._id.toString());
+    });
+    const hiddenIdArray = Array.from(hideFromUser);
+    const users = await User.find({
+      _id: { $nin: hiddenIdArray },
+    })
+      .skip(skip)
+      .limit(limit);
+    res.send(users);
   } catch (err) {
     res.status(400).send("ERROR :" + err.message);
     console.log(err);
